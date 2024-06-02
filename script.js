e2a4ef9ace970e33ba6219f372156404a03ac12b
shamo74 <send.shamo@gmail.com>
2024-06-03 01:25:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// تحديد الكلمات المترجمة
+const dictionary = {
+    "مرحبا": "اهلين",
+    "كيفك": "بخير",
+    // يمكنك إضافة المزيد من الكلمات وترجماتها هنا
+};
+
+// التحقق من تغيير النص في حقل الإدخال
+document.getElementById('inputField').addEventListener('input', function() {
+    const inputText = this.value.trim();
+    const outputField = document.getElementById('outputField');
+    
+    if (inputText !== '') {
+        // التحقق مما إذا كانت الكلمة موجودة في القاموس
+        if (dictionary.hasOwnProperty(inputText)) {
+            outputField.textContent = dictionary[inputText];
+        } else {
+            outputField.textContent = 'لا توجد ترجمة';
+        }
+    } else {
+        outputField.textContent = '';
+    }
+});
